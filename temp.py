af2565f5de3781f75c0d36b5edc4cc7d4760dec7
Timofey Yakovishin <timayakovishin@gmail.com>
2022-05-13 14:15:41 +0300

    last upd

--- temp.py
+++ temp.py
@@ -1,22 +1,34 @@
-import imp
-import logging
-import datetime
-import pandas
-import time
-import psycopg2
-import requests
-import json
-import telegram
-import os
-from dotenv import load_dotenv
-from config import tg_access_token
+# import imp
+# import logging
+# import datetime
+# import pandas
+# import time
+# import psycopg2
+# import requests
+# import json
+# import telegram
+# import os
+# from dotenv import load_dotenv
+# from config import tg_access_token
 
-print(pandas.__version__)
-print(psycopg2.__version__)
-print(requests.__version__)
-print(json.__version__)
-print(telegram.__version__)
+# print(pandas.__version__)
+# print(psycopg2.__version__)
+# print(requests.__version__)
+# print(json.__version__)
+# print(telegram.__version__)
 
-#print([x for x in os.listdir(os.getcwd()) if x.find('vk') > -1][0])
-load_dotenv()
-print(tg_access_token)
+# #print([x for x in os.listdir(os.getcwd()) if x.find('vk') > -1][0])
+# load_dotenv()
+# print(tg_access_token)
+
+from sqldb import DataBase_Con
+from config import db_db_name, db_host, db_password, db_user
+
+
+test_con = DataBase_Con(db_host,db_user,  db_password,  db_db_name)
+test_con.connection_on()
+content = set(test_con.GetIDPostsSQL())
+post = set(test_con.GetIDPostsSQL())
+
+print(len(post & content))
+
--- updData.py
+++ updData.py
@@ -1,18 +1,17 @@
 from __future__ import print_function
-import json
-import requests
-import psycopg2
-from psycopg2.extensions import AsIs
 import pandas as pd
-from time import sleep
-from datetime import datetime, timedelta
+from config import db_db_name, db_host, db_password, db_user
+
 
 #--------MY moduls
 from ETLAndDB import getVkJson # get data from VK
 from ETLAndDB import dateStamper, changeSignOwnerID,getPhotoMetaData,getAttachments,getPhotoID, getSizes, convert_asis, dateSTAMP, takeNewestPosts, currentDate
-from sqldb import GetDataPostsSQL, sendDataContentSQL, sendDataPostsSQL,getIndexesFromMainDB
+from sqldb import DataBase_Con, GetDataPostsSQL, sendDataContentSQL, sendDataPostsSQL,getIndexesFromMainDB
 
-main_list = getIndexesFromMainDB()
+
+connect_db = DataBase_Con(db_host,db_user,  db_password,  db_db_name)
+connect_db.connection_on()
+main_list = connect_db.getIndexesFromMainDB()
 df_from_vk = getVkJson(main_list)
 
 
@@ -70,18 +69,22 @@ save_date = pd.Series(currentDate(), index=range(len(frame_main_df)))
 frame_main_df.loc[:, 'save_date'] = save_date
 
 
-temp_df = GetDataPostsSQL()
+temp_df = connect_db.GetIDPostsSQL()
 try:
     new_df = takeNewestPosts(temp_df, frame_main_df)
 
     for_content_db = new_df[['content_id', 'owner_id', 'public_date', 'save_date']]
     for_post_db = new_df[[ 'url', 'text','content_id']]
     for_content_db = for_content_db.astype({'public_date':str, 'save_date':str})
-    sendDataContentSQL(for_content_db)
-    sendDataPostsSQL(for_post_db)
+    connect_db.sendDataContentSQL(for_content_db)
+    connect_db.sendDataPostsSQL(for_post_db)
     print("[INFO] Databases have been updated")
 except Exception as _ex:
     print("[INFO] You don't have a new data", _ex)
+
+
+print('The end')
+
 
 
 
